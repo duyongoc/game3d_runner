c9b9c620c65e3f88601b341d139975a3931a0861
duyongoc <ongocduy98@gmail.com>
2020-06-09 00:36:29 +0700

    + Spawn the hole and make it move to random position

---
+++ Assets/Scripts/TheHole/TheHole.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TheHole : MonoBehaviour
+{
+    public ScriptTheHole scriptTheHole;
+
+    private float moveSpeed;
+    private float distanceRadius;
+    private Vector3 target;
+
+    void Start()
+    {
+        moveSpeed = scriptTheHole.moveSpeed;
+        distanceRadius = scriptTheHole.distanceRadius;
+
+        target = GetRandomPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+        float distance = Vector3.Distance(transform.position, target);
+        if(distance <= 0.1f)
+        {
+            target = GetRandomPoint();
+        }
+
+
+    }
+
+    private Vector3 GetRandomPoint()
+    {
+        NavMeshHit hit;
+        do
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * distanceRadius;
+            randomDirection += transform.position;
+            NavMesh.SamplePosition(randomDirection, out hit, distanceRadius, 1);
+        }
+        while(hit.position.x == Mathf.Infinity && hit.position.y == Mathf.Infinity);
+        
+        return hit.position;
+    }
+}
